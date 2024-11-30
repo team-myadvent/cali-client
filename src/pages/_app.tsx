20229a0439c16c80723a18799fa3d837376ab20b
seulgi <sgsg9447@gmail.com>
2024-11-30 23:39:42 +0900

    feat: Apply global styles and set up color variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import "@/styles/globals.css";
+import GlobalStyle from "@/styles/GlobalStyle";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <Script
         src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js"
         integrity="sha384-DKYJZ8NLiK8MN4/C5P2dtSmLQ4KwPaoqAfyA/DfmEc1VDxu4yyC7wy6K1Hs90nka"
---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  lemon: "#FFFDCE",
+  brown: {
+    5: "#5A4B45",
+    4: "#968984",
+    3: "#B3A8A2",
+    2: "#DCD7D4",
+    1: "#F7F3F1",
+  },
+} as const;
