4c40ff224330c77390b2c17d32f1d0d9a8e5edc5
seulgi <sgsg9447@gmail.com>
2024-12-08 01:21:57 +0900

    feat: add a default thumbnail image and modify config settings about Next image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cali-bucket.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/media/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
