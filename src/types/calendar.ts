c79c7f450c5ba8adfdf13f974f8818e2de763c09
seulgi <sgsg9447@gmail.com>
2024-12-08 16:50:27 +0900

    refactor: move a type files

--- src/types/calendar.ts
+++ src/types/calendar.ts
@@ -12,6 +12,16 @@ export interface CalendarCardItem {
   calendar_dt: string;
   title: string;
   comment: string;
-  youtube_music_link: string;
+  comment_detail: string;
+  youtube_video_id: string;
   calendar_thumbnail: string;
+}
+
+export interface UpdateCalendarCardItem {
+  title?: string;
+  comment?: string;
+  comment_detail?: string;
+  youtube_video_id?: string;
+  thumbnail_file?: File;
+  youtube_thumbnail_link?: string;
 }
